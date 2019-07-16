a7f7236b034b270de3419f8a8573bc2faec9774d
srid-infra <33086498+srid-infra@users.noreply.github.com>
2019-07-16 12:09:01 +0530

    Update Visit.java

--- src/main/java/org/springframework/samples/petclinic/visit/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -111,4 +111,10 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IO
   engine.eval(input); // Noncompliant
 }
 
+synchronized (this.mon1) {  // threadB can't enter this block to request this.mon2 lock & release threadA
+    synchronized (this.mon2) {
+        this.mon2.wait();  // Noncompliant; threadA is stuck here holding lock on this.mon1
+    }
+}
+
 
